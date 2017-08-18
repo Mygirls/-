775004eca6347de8d5c6760026440db1c7271a29
JQ <jq@mjqdeMacBook-Pro.local>
2017-08-18 11:38:32 +0800

    test

---
+++ 合并冲突/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  合并冲突
+//
+//  Created by JQ on 2017/8/18.
+//  Copyright © 2017年 majq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
